51ac5cb5fb2c49676eea40241f5bbb785f9125d9
irshadka007 <irshadka1996@gmail>
2020-01-04 16:52:38 +0530

    Updates

---
+++ precache-manifest.c97ba994fe873e1ab76e7d1369c78283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433792f1dc65ebd4b407794bd02403f2",
+    "url": "/sketchmonk/index.html"
+  },
+  {
+    "revision": "8b39e5fd93959d89ac0d",
+    "url": "/sketchmonk/static/css/main.679f01ba.chunk.css"
+  },
+  {
+    "revision": "3309005854258c7ce132",
+    "url": "/sketchmonk/static/js/2.6bf10a59.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sketchmonk/static/js/2.6bf10a59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b39e5fd93959d89ac0d",
+    "url": "/sketchmonk/static/js/main.80032f24.chunk.js"
+  },
+  {
+    "revision": "0512dbe022734146db20",
+    "url": "/sketchmonk/static/js/runtime-main.cdd4d901.js"
+  }
+]);
