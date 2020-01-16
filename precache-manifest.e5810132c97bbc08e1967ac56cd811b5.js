c5034c23c41ffe1aa9954a2db8c607b264695edb
irshadka007 <irshadka1996@gmail>
2020-01-16 16:09:45 +0530

    Updates

---
+++ precache-manifest.e5810132c97bbc08e1967ac56cd811b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a386c84f9094133f1d6630d9606651",
+    "url": "/sketchmonk/index.html"
+  },
+  {
+    "revision": "70138e77b34b9734ec65",
+    "url": "/sketchmonk/static/css/main.1177d173.chunk.css"
+  },
+  {
+    "revision": "0f0421dbb054bdd91df3",
+    "url": "/sketchmonk/static/js/2.1d4231f8.chunk.js"
+  },
+  {
+    "revision": "b6d73ad891485ea8d0d63ad8166c2990",
+    "url": "/sketchmonk/static/js/2.1d4231f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70138e77b34b9734ec65",
+    "url": "/sketchmonk/static/js/main.7fe23b53.chunk.js"
+  },
+  {
+    "revision": "0512dbe022734146db20",
+    "url": "/sketchmonk/static/js/runtime-main.cdd4d901.js"
+  },
+  {
+    "revision": "1bf9f1ceccc426054dc72d450b5faba5",
+    "url": "/sketchmonk/static/media/logo_text.1bf9f1ce.png"
+  },
+  {
+    "revision": "f99ff4c6cddc817c36d6d29d200b7c90",
+    "url": "/sketchmonk/static/media/logo_vertical.f99ff4c6.png"
+  }
+]);
